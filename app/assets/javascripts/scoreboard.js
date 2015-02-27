6785287fdaa94725784cf8151aaca65b752ab382
Damien Roche <damien.roche@onthebeach.co.uk>
2015-02-27 16:23:07 +0000

    very crappy implementation of javascript scoring

--- app/assets/javascripts/scoreboard.js
+++ app/assets/javascripts/scoreboard.js
@@ -1,5 +1,8 @@
 var Scoreboard = (function($, _) {
 
+  // todo: major refactorings!
+  // todo: extract player round into object
+
   var Scoreboard = function(){
     // todo: set properties
     this.bidElement = $(".game_rounds_player_rounds_bid input");
@@ -16,21 +19,19 @@ var Scoreboard = (function($, _) {
     var _this = this;
 
     this.bidElement.on("change keyup", function(element){
-      _this.calculate_score($(element.target));
+      _this.calculateScore($(element.target));
+    });
+
+    this.contractElement.on("change keyup", function(element){
+      _this.calculateScore($(element.target));
     });
   }
 
-  Scoreboard.prototype.calculate_score = function(element) {
-    if(this.rowOfElementComplete(element)) {
-      // todo: calc
-    }
-  }
+  Scoreboard.prototype.calculateScore = function(element) {
+    var playerRound = this.playerRound(element);
+    var score = this.playerRoundScore(playerRound);
 
-  Scoreboard.prototype.rowOfElementComplete = function(element) {
-    var row = this.rowOfElement(element);
-
-    // todo: loop through row elements and check that all
-    // have data
+    this.setScoreOnPlayerRound(playerRound, score);
   }
 
   Scoreboard.prototype.rowOfElement = function(element) {
@@ -38,6 +39,57 @@ var Scoreboard = (function($, _) {
     var elements = row.find(".game_rounds_player_rounds_bid input, .game_rounds_player_rounds_contracts input");
 
     return elements;
+  }
+
+  Scoreboard.prototype.playerRound = function(element) {
+    var playerRoundNum = $(element).attr("id").match(/player_rounds_attributes_(\d+)/)[1];
+
+    if($(element).parent().hasClass('game_rounds_player_rounds_bid')) {
+      return {
+        bid: $(element),
+        contracts: $(element).parent().parent().next("td").find("input"),
+        num: playerRoundNum
+      }
+    } else {
+      return {
+        bid: $(element).parent().parent().prev("td").find("input"),
+        contracts: $(element),
+        num: playerRoundNum
+      }
+    }
+  }
+
+  Scoreboard.prototype.playerRoundScore = function(playerRound) {
+    var previousScore = this.previousScore(playerRound);
+    var bid = parseInt(playerRound.bid[0].value);
+    var contracts = parseInt(playerRound.contracts[0].value);
+    var score = contracts;
+
+    if(bid == contracts) {
+      score += 10;
+    }
+
+    return previousScore + score;
+  }
+
+  Scoreboard.prototype.previousScore = function(playerRound) {
+    var previousRound = $(playerRound.bid).closest("tr").prevAll(".player_rounds")[0];
+
+    if(previousRound != undefined) {
+      var roundInputs = $(previousRound).find("input.integer");
+      var previousRoundElement = roundInputs[playerRound.num * 2];
+      var scoreElement = $(previousRoundElement).closest("td").nextAll(".score").first();
+
+      return parseInt(scoreElement.text());
+    } else {
+      return 0;
+    }
+  }
+
+  Scoreboard.prototype.setScoreOnPlayerRound = function(playerRound, score) {
+    var scoreElement = $(playerRound.bid).closest("td").nextAll(".score").first();
+
+    scoreElement.text(score);
   }
 
   return Scoreboard;
