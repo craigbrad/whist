33f8b1e5ef9ceef2f427372ddd405470bc9e03ce
Damien Roche <damien.roche@onthebeach.co.uk>
2015-03-11 12:46:12 +0000

    add date to games

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150306154041) do
+ActiveRecord::Schema.define(version: 20150311124340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20150306154041) do
     t.integer  "number_of_players"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.date     "date"
   end
 
   create_table "games_players", force: true do |t|
